464ec5f6ee61cc2807b26a5c309959a21b0243c3
Bethany Armitage <bethgrace5@gmail.com>
2018-01-14 12:22:59 -0800

    fix deploy custom domain

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
-echo "www.ableautotech.com" > dist/CNAME
-ng build --prod --base-href "http://www.ableautotech.com"
+# Build project
+echo "************************************************";
+echo "building...";
+ng build --prod --base-href "http://www.ableautotech.com";
+echo "";
 
+# Setup CNAME to point to custom domain
+echo "************************************************";
+echo "www.ableautotech.com" > dist/CNAME;
+echo "";
+
+# Publish project
+echo "************************************************";
+echo "publishing...";
+ngh;
+echo "done.";
