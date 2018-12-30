0322d3ef2601965d746f6f66c564ff1f127a142f
Bethany Armitage <bethgrace5@gmail.com>
2018-12-30 14:05:37 -0800

    fix directions, appointment and telephone links, test image url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
-  isMenuCollapsed=true;
-  onActivate(e) {
+  public title = 'app';
+  public googleUrl = "https://www.google.com/maps/dir/?api=1&origin=Current%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&destination_place_id=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
+  public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
+  public telephoneLink = "tel:+16616370669"
+
+  public isMenuCollapsed=true;
+  public onActivate(e) {
     window.scrollTo(0, 0);
   }
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -5,4 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 
-export class DashboardComponent { }
+export class DashboardComponent {
+  public googleUrl = "https://www.google.com/maps/dir/?api=1&origin=Current%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&destination_place_id=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
+  public telephoneLink = "tel:+16616370669"
+  public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
+}
