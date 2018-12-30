0c1d8fdc5e8c1b0097c01863ffcceab9fe10276c
Bethany Armitage <bethgrace5@gmail.com>
2018-12-30 14:17:08 -0800

    fix google url, appointment link in action buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'app';
-  public googleUrl = "https://www.google.com/maps/dir/?api=1&origin=Current%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&destination_place_id=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
+  public googleUrl = "https://www.google.com/maps/dir/?api=1&saddr=My%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&daddr=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
   public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
   public telephoneLink = "tel:+16616370669"
 
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
 })
 
 export class DashboardComponent {
-  public googleUrl = "https://www.google.com/maps/dir/?api=1&origin=Current%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&destination_place_id=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
+  public googleUrl = "https://www.google.com/maps/dir/?api=1&saddr=My%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&daddr=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
   public telephoneLink = "tel:+16616370669"
   public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
 }
