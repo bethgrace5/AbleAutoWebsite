1fd26fa489c7f65679e1e09dc10723d5860b1eff
Bethany Armitage <bethgrace5@gmail.com>
2020-12-31 17:50:19 -0800

    covid copy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'app';
   public googleUrl = "https://www.google.com/maps/dir/?api=1&saddr=My%20Location&destination=Able+Auto+and+Truck+Repair,+Fruitvale+Avenue+%231,+Bakersfield,+CA&daddr=ChIJzRQ-WEFC6oARnf-v_gT18YA&travelmode=driving"
-  public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
   public telephoneLink = "tel:+16616370669"
+  public appointmentUrl = "http://www.mitchell1crm.com/crmutilities/RequestAppointment.aspx?v=SureCritic&w=959133"
 
   public isMenuCollapsed=true;
   public onActivate(e) {
